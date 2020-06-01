be49f889792f352fe7bb566ea8771982dd8ac7e6
FedorovaNK <64545483+FedorovaNK@users.noreply.github.com>
2020-06-02 00:37:14 +0300

    Add files via upload

---
+++ TorgObject.DesktopClient/InfrastructureServices/Repositories/CachedReadOnlyPechatProductRepository.cs
@@ -0,0 +1,33 @@
+﻿using TorgObject.ApplicationServices.Ports.Cache;
+using TorgObject.DomainObjects;
+using TorgObject.DomainObjects.Ports;
+using TorgObject.DomainObjects.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorgObject.InfrastructureServices.Repositories
+{
+    public class CachedReadOnlyPechatProductRepository : ReadOnlyPechatProductRepositoryDecorator
+    {
+        private readonly IDomainObjectsCache<PechatProduct> _pechatproductsCache;
+
+        public CachedReadOnlyPechatProductRepository(IReadOnlyPechatProductRepository pechatproductRepository, 
+                                                     IDomainObjectsCache<PechatProduct> pechatproductsCache)
+            : base(pechatproductRepository)
+            => _pechatproductsCache = pechatproductsCache;
+
+        public async override Task<PechatProduct> GetPechatProduct(long id)
+            => _pechatproductsCache.GetObject(id) ?? await base.GetPechatProduct(id);
+
+        public async override Task<IEnumerable<PechatProduct>> GetAllPechatProducts()
+            => _pechatproductsCache.GetObjects() ?? await base.GetAllPechatProducts();
+
+        public async override Task<IEnumerable<PechatProduct>> QueryPechatProducts(ICriteria<PechatProduct> criteria)
+            => _pechatproductsCache.GetObjects()?.Where(criteria.Filter.Compile()) ?? await base.QueryPechatProducts(criteria);
+
+    }
+}
---
+++ TorgObject.DesktopClient/InfrastructureServices/Repositories/NetworkPechatProductRepository.cs
@@ -0,0 +1,46 @@
+﻿using TorgObject.ApplicationServices.Ports.Cache;
+using TorgObject.DomainObjects;
+using TorgObject.DomainObjects.Ports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace TorgObject.InfrastructureServices.Repositories
+{
+    public class NetworkPechatProductRepository : NetworkRepositoryBase, IReadOnlyPechatProductRepository
+    {
+        private readonly IDomainObjectsCache<PechatProduct> _pechatproductCache;
+
+        public NetworkPechatProductRepository(string host, ushort port, bool useTls, IDomainObjectsCache<PechatProduct> pechatproductCache)
+            : base(host, port, useTls)
+            => _pechatproductCache = pechatproductCache;
+
+        public async Task<PechatProduct> GetPechatProduct(long id)
+            => CacheAndReturn(await ExecuteHttpRequest<PechatProduct>($"pechatproducts/{id}"));
+
+        public async Task<IEnumerable<PechatProduct>> GetAllPechatProducts()
+            => CacheAndReturn(await ExecuteHttpRequest<IEnumerable<PechatProduct>>($"pechatproducts"), allObjects: true);
+
+        public async Task<IEnumerable<PechatProduct>> QueryPechatProducts(ICriteria<PechatProduct> criteria)
+            => CacheAndReturn(await ExecuteHttpRequest<IEnumerable<PechatProduct>>($"pechatproducts"), allObjects: true)
+               .Where(criteria.Filter.Compile());
+
+        private IEnumerable<PechatProduct> CacheAndReturn(IEnumerable<PechatProduct> pechatproducts, bool allObjects = false)
+        {
+            if (allObjects)
+            {
+                _pechatproductCache.ClearCache();
+            }
+            _pechatproductCache.UpdateObjects(pechatproducts, DateTime.Now.AddDays(1), allObjects);
+            return pechatproducts;
+        }
+
+        private PechatProduct CacheAndReturn(PechatProduct pechatproduct)
+        {
+            _pechatproductCache.UpdateObject(pechatproduct, DateTime.Now.AddDays(1));
+            return pechatproduct;
+        }
+    }
+}
